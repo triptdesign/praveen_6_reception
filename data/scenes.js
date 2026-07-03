0955a01191f55772a3147a75204faafa1aec23bf
triptdesign <triptdesign@gmail.com>
2026-07-04 05:26:15 +0530

    Initial commit

---
+++ data/scenes.js
@@ -0,0 +1,12 @@
+window.PANORAMA_SCENES = [
+    {
+        "id": "option_1",
+        "title": "Option_1",
+        "file": "option_1.png"
+    },
+    {
+        "id": "option_2",
+        "title": "Option_2",
+        "file": "option_2.png"
+    }
+];
